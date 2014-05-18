3c65f6cfd89519504a178829379e908dcc73b67e
Pan W <panthongw@gmail.com>
2014-05-18 11:51:30 -0700

    added some edit post code

--- client/router.js
+++ client/router.js
@@ -7,12 +7,17 @@ Router.map(function(){
   
   this.route('postPage', {
   	path: '/posts/:_id',
-  	data: function() { return Posts.findOne(this.params._id); }
+  	data: function() { return Posts.findOne(this.params._id) }
   });
 
   this.route('postSubmit', {
   	path: '/submit',
   	controller: 'PostCreateController'
+  });
+
+  this.route('postEdit', {
+  	path: 'posts/:_id/edit',
+  	data: function() { return Posts.findOne(this.params._id) }
   });
 });
 
